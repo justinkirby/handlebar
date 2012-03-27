f1a17cf26779076947d83074178b55638edd9790
Justin Kirby <jkirby@voalte.com>
2012-03-27 10:57:30 -0400

    adding publish.el to create README.html

---
+++ docs/publish.el
@@ -0,0 +1,23 @@
+(defun handlebar-publish ()
+
+
+  (require 'org-install)
+  (require 'org-publish)
+
+
+
+  (setq org-publish-project-alist
+        '(
+          ("handlebar"
+           :publishing-directory "html"
+           :base-directory "."
+           :recursive t
+           :publishing-function org-publish-org-to-html
+           :auto-preamble t
+           :sub-superscript nil
+           )
+          )
+        )
+
+  (org-publish (assoc "handlebar" org-publish-project-alist) t)
+  )
